c616d0ade6138255c1a47089aae004b557ea3b72
zouyangming <2327972001@qq.com>
2022-06-03 12:37:11 +0800

    增加到期自动检测

---
+++ ruoyi-admin/src/main/java/com/ruoyi/task/TimingTaskImpl.java
@@ -0,0 +1,36 @@
+package com.ruoyi.task;
+
+import com.ruoyi.domain.ZymProduct;
+import com.ruoyi.service.IZymProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author ZouYangMing
+ * @deprecated 定时任务
+ */
+@Component("timingTask")
+public class TimingTaskImpl {
+
+    @Autowired
+    private IZymProductService zymProductService;
+
+    public void start() {
+        List<ZymProduct> zymProductList = zymProductService.selectZymProductList(new ZymProduct());
+        for (ZymProduct zymProduct : zymProductList) {
+            Date now = new Date();
+            //到期检测
+            if (now.getTime() > zymProduct.getEndtime().getTime()) {
+                ZymProduct zymProduct1 = new ZymProduct();
+                zymProduct1.setId(zymProduct.getId());
+                zymProduct1.setStatus("1");
+                zymProductService.updateZymProduct(zymProduct1);
+            }
+        }
+    }
+
+}
